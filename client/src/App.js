40594ba69507ecfdb9c7b616e217ddad272ab354
simku22 <simku22@uw.edu>
2024-05-13 13:05:46 -0700

    added tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 //data will be the string we send from our server
 const apiCall = () => {
-  axios.get('http://localhost:3001/').then((data) => {
+  axios.get('http://localhost:3000/').then((data) => {
     //this console.log will be in our frontend console
     console.log(data);
   })
@@ -13,20 +13,7 @@ const apiCall = () => {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={apiCall}>Make API Call</button>
     </div>
   );
 }
