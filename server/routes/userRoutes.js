401906002a3d0cdcd4acc3157580885d20d2d2b5
simku22 <simku22@uw.edu>
2024-05-23 01:48:22 -0700

    missing revised user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -39,23 +39,18 @@ function validateEmail(email) {
 }
 
 router.get('/past-events', async(req, res) => {
-    try{
-        const { email } = req.body;
-
-        // comment this out while debugging
-        /*
-        if(!req.session.account) {
-            return res.status(401).json({ error: 'User not authenticated' })
+    if (req.session.isAuthenticated) {
+        try{
+            const result = await req.sql.query(`SELECT user_id FROM users WHERE user_email = '${req.session.account.username}'`);
+            const user_id = result.recordset[0].user_id;
+            const resp = await req.sql.query(`SELECT * FROM events WHERE user_id = ${user_id} ORDER BY created DESC`)
+            const users_meetings = resp.recordset;
+            res.status(200).json(users_meetings);
+        } catch (error) {
+            res.status(500).json({ status: 'error', error: error.message });
         }
-        */
-        const result = await req.sql.query(`SELECT user_id FROM users WHERE user_email = '${email}'`);
-        const user_id = result.recordset[0].user_id;
-
-        const resp = await req.sql.query(`SELECT * FROM events WHERE user_id = ${user_id} ORDER BY created DESC`)
-        const users_meetings = resp.recordset;
-        res.status(200).json(users_meetings);
-    } catch (error) {
-        res.status(500).json({ status: 'error', error: error.message });
+    } else {
+        res.status(500).json({ status: 'not logged in'});
     }
 })
 
